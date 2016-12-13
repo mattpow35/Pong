f2f590a21e52cf8cc5860385f5ec7fd7c0ef11da
mattpow35 <mattpowley@Matts-MacBook-Pro.local>
2016-12-12 17:48:09 -0700

    Working on making the home screen
    Need to get the start screen to transition to the game scene when I
    press the sprite node

---
+++ Pong/StartScreen.swift
@@ -0,0 +1,32 @@
+//
+//  StartScreen.swift
+//  Pong
+//
+//  Created by Matt Powley on 12/12/16.
+//  Copyright © 2016 Matt Powley. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class StartScreen: SKScene
+{
+    
+    
+    var welcomeLabel = SKLabelNode()
+    var startButton = SKSpriteNode()
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+
